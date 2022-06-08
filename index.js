6a7a38d68544266b8a60a418e5f42d27affca41e
FALLELH <100312717+FALLELH@users.noreply.github.com>
2022-06-08 13:45:40 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+
+const hpac = document.querySelector('.header .nav-bar .nav-list .hpac');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hpac.addEventListener('click', () => {
+	hpac.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hpac.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
